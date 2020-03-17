76365e536d2a63492ad2146352149d2e3888958d
ArtemYurchenko <48692xp@gmail.com>
2020-03-18 00:51:35 +0300

    Revert "fixed"
    This reverts commit 8bc56e56299b2a7603baab698cc341d2b658cc16.

--- 02/myparser.cpp
+++ 02/myparser.cpp
@@ -8,47 +8,31 @@ void MyParser::register_on_number_callback(OnNumber callback){
 }
 void MyParser::register_on_str_callback(OnString callback){
     on_str_callback = callback;
+    return;
 }
 
-void MyParser::register_on_start_callback(StartEnd callback){
-    parse_start = callback;
-}
-
-void MyParser::register_on_end_callback(StartEnd callback){
-    parse_end = callback;
-}
-
-MyParser::MyParser(){
-    parse_start = nullptr;
-    parse_end = nullptr;
-    on_number_callback = nullptr;
-    on_str_callback = nullptr;
-}
 void MyParser::parse(const char* text){
-    if ((parse_end == nullptr) || (parse_start == nullptr) || 
-        (on_str_callback == nullptr) || (on_number_callback == nullptr)){
-            std::cerr << "Uninitialized callback";
-            return;
-        }
-    parse_start();
+    std::cout << "Parsing starts\n";
     while ((*text) != '\0'){
-        std::string buf;
+        char* buf = new char[strlen(text) + 1];
+        size_t i = 0;
         if (isdigit(*text)){
             while (!isspace(*text) && (*text != '\0')){
-                buf.push_back(*text++);
+                buf[i++] = *text++;
             }
-            buf.push_back('\0');
-            on_number_callback(atoi(buf.c_str()));
+            buf[i] = '\0';
+            on_number_callback(buf);
+            delete []buf;
         } else if (isalpha(*text)){
             while (!isspace(*text) && (*text != '\0')){
-                buf.push_back(*text++);
+                buf[i++] = *text++;
             }
-            buf.push_back('\0');
-            on_str_callback(buf.c_str());
+            buf[i] = '\0';
+            on_str_callback(buf);
+            delete []buf;
         } else if (isspace(*text)){
             ++text;
         }
     }
-    parse_end();
-    return;   
+    std::cout << "Parsing has ended\n";
 }
--- 02/myparser.h
+++ 02/myparser.h
@@ -1,17 +1,11 @@
 #pragma once
-using OnNumber = void (*)(const int number);
+using OnNumber = void (*)(const char* number);
 using OnString = void (*)(const char* str);
-using StartEnd = void (*)();
 class MyParser{
     OnNumber on_number_callback;
     OnString on_str_callback;
-    StartEnd parse_end;
-    StartEnd parse_start;
 public:
-    MyParser();
     void parse(const char* text);
     void register_on_number_callback(OnNumber callback);
     void register_on_str_callback(OnString callback);
-    void register_on_start_callback(StartEnd callback);
-    void register_on_end_callback(StartEnd callback);
 };
